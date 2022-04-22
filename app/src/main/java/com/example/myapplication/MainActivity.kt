81f4aea6dafbe28eff8c5c0185319d8fa18de237
Egor <niketen91@mail.ru>
2022-04-22 16:06:26 +0300

    Zadanie 1 Dz14

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,12 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
-class MainActivity : AppCompatActivity() {
-    val TAG = "MainActivity"
+    class MainActivity : AppCompatActivity() {
+
+        var userlist: MutableList<Person> = mutableListOf()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.e(TAG, "Hello World")
+
+
+        addUser("Иван", "Иванов",31)
+        addUser("Никита", "Иванов",30)
+        addUser("костя", "Иванов",43)
+
+        output()
+    }
+    private fun addUser(name:String,secondName:String, age: Int ){
+        val user = Person(name,secondName,age)
+        userlist.add(user)
+    }
+    private fun output(){
+    for (user in userlist){
+        println(user)
+    }
     }
 }
+
