56f1259d9db9ac537fdf933c12e0f1b0d07a4ddd
Egor <niketen91@mail.ru>
2022-04-23 23:41:14 +0300

    Zadanie 4 Dz15

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,17 +11,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var a: Int
-        var c = 0
-        val scn = Scanner(System.`in`)
-        if (scn.hasNextInt()) {
-            a = scn.nextInt()
-            while (a != 0) {
-                c += a % 10
-                a /= 10
-            }
-            println("The sum is $c")
-        } else println("Error. Try again")
+        val num = 100
+        var summary = 0
+
+        for (i in 1 .. num) {
+            summary += i
+        }
+        println(summary)
     }
 }
 
