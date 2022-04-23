68190cfd1bd5af596d2a0f54fb1d402952c8386a
Egor <niketen91@mail.ru>
2022-04-23 23:07:43 +0300

    Zadanie 1 Dz15

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,33 +1,51 @@
 package com.example.myapplication
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.annotation.RequiresApi
 
-    class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
-        var userlist: MutableList<Person> = mutableListOf()
+    var userlist: MutableList<Person> = mutableListOf()
 
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val user1 = Person("Ваван", "Иванов", 31)
+        val user2 = Person("Антон", "Иванов", 17)
+        val user3 = Person("Дима", "Иванов", 43)
 
-
-        addUser("Иван", "Иванов",31)
-        addUser("Никита", "Иванов",30)
-        addUser("костя", "Иванов",43)
-
-        output()
+        add(user1)
+        add(user2)
+        add(user3)
+        showList()
+        deleteByAge()
     }
-    private fun addUser(name:String,secondName:String, age: Int ){
-        val user = Person(name,secondName,age)
+
+    private fun add (user: Person) {
         userlist.add(user)
     }
-    private fun output(){
-    for (user in userlist){
-        println(user)
+    private fun showList () {
+            userlist.sortBy { it.name }
+        for (person in userlist) {
+            println(toString(person))
+        }
     }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun deleteByAge() {
+        userlist.removeIf{ it.age <= 18 }
+        for (person in userlist) {
+            println(toString(person))
+        }
+    }
+
+    fun toString(person: Person): String {
+        return "Name: ${person.name}  Surname:  ${person.secondName}  Age:  ${person.age}"
     }
 }
+
 
--- app/src/main/java/com/example/myapplication/Person.kt
+++ app/src/main/java/com/example/myapplication/Person.kt
@@ -1,3 +1,6 @@
 package com.example.myapplication
 
-   data class Person( val   name:String, val secondName:String,val  age: Int)
+   data class Person( val   name:String, val secondName:String,val  age: Int) : Comparable<Person> {
+      override fun compareTo(other: Person): Int = name.compareTo(other.name)
+      override fun toString(): String = "$name ($secondName,$age)"
+   }
