fce8e1a7d073fb683cfc1641bd132986bd69d394
Egor <niketen91@mail.ru>
2022-04-23 23:40:32 +0300

    Zadanie 3 Dz15

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,21 +4,24 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.annotation.RequiresApi
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val randomValue = (5..155).random()
-        println(randomValue)
-        val interval = 25..100
-
-        if (interval.contains(randomValue)) {
-            println("Contains")
-        } else {
-            println("Doesn't contains")
-        }
+        var a: Int
+        var c = 0
+        val scn = Scanner(System.`in`)
+        if (scn.hasNextInt()) {
+            a = scn.nextInt()
+            while (a != 0) {
+                c += a % 10
+                a /= 10
+            }
+            println("The sum is $c")
+        } else println("Error. Try again")
     }
 }
 
