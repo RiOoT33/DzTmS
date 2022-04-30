a47e761dd976cd76d62785e3081f21f654b82825
Egor <niketen91@mail.ru>
2022-05-01 02:12:09 +0300

    Zadanie 1 With sweat and blood Dz 16

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,15 +1,34 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
-    val TAG = "MainActivity"
+    private lateinit   var bindingClass : ActivityMainBinding
+    private val TAG = "Info"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Log.e(TAG, "Hello World")
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+        bindingClass.buttonValue.setOnClickListener {
+
+            val resultA =  bindingClass.inputA.text.toString().toInt()
+            Log.i(TAG,   "Ввели число  A = $resultA")
+            val resultB = bindingClass.inputB.text.toString().toInt()
+            Log.i(TAG,   "Ввели число  B = $resultB")
+            val  multiplication = resultA * resultB
+            val summa = resultA + resultB
+            when(resultA % 2 == 0){
+                true ->
+                    bindingClass.result.text = "Результат = $multiplication"
+              false -> bindingClass.result.text = "Результат = $summa"
+            }
+        }
     }
 }
+
+
