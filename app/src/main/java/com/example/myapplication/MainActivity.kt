063cee7aa751f7c62d9abb50f1d9e581e4bf2955
Egor <niketen91@mail.ru>
2022-05-01 03:34:45 +0300

    Task 3 Dz 16

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,27 +2,40 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit   var bindingClass : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        max(1, 2, 3)
-    }
-
-    private fun max(a: Int, b: Int, c: Int) {
-        val plus = a + b + c
-        val multiplay = a * b * c
-        when {
-            plus == multiplay -> Log.i("Value","Значения равны")
-            plus > multiplay -> Log.i("Value","$plus > $multiplay")
-            plus < multiplay -> Log.i("Value","$plus < $multiplay")
-
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+        bindingClass.buttonClick.setOnClickListener {
+            val student =  bindingClass.studentGrade.text.toString().toInt()
+                Log.i("student","Оценка = $student")
+            when(student){
+                in 0..19 -> {bindingClass.result.visibility = View.VISIBLE
+                             bindingClass.result.text = "Ваша оценка F"}
+                in 20..39 ->{bindingClass.result.visibility = View.VISIBLE
+                             bindingClass.result.text = "Ваша оценка E"}
+                in 40..59 ->{bindingClass.result.visibility = View.VISIBLE
+                             bindingClass.result.text = "Ваша оценка D"}
+                in 60..74 ->{bindingClass.result.visibility = View.VISIBLE
+                             bindingClass.result.text = "Ваша оценка C"}
+                in 75..89 ->{bindingClass.result.visibility = View.VISIBLE
+                             bindingClass.result.text = "Ваша оценка B"}
+                in 90..100 ->{bindingClass.result.visibility = View.VISIBLE
+                              bindingClass.result.text = "Ваша оценка A"}
+                else -> {bindingClass.result.visibility = View.VISIBLE
+                         bindingClass.result.text = "Такой оценки нет"}
+            }
         }
     }
 }
+
 
 
