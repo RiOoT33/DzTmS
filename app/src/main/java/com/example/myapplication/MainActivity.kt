5cd982d3f51c1e96e3ccbd093a5e881f80b5844f
Egor <niketen91@mail.ru>
2022-05-02 03:24:56 +0300

    Task 2 Dz 17

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,14 +2,35 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.os.CountDownTimer
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    val TAG = "MainActivity"
-
+    private var timer: CountDownTimer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.e(TAG, "Hello World")
+
+        val button: Button = findViewById(R.id.btnBegin)
+        button.setOnClickListener {
+            val textEditor: TextView = findViewById(R.id.secondsReceiver)
+            startTimer(textEditor.text.toString().toInt())
+        }
+    }
+
+    fun startTimer (seconds: Int) {
+        timer?.cancel()
+        val textEditor: TextView = findViewById(R.id.secondsReceiver)
+        timer = object: CountDownTimer(seconds.toLong()*1000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                textEditor.text = (millisUntilFinished/1000).toString()
+            }
+
+            override fun onFinish() {
+                textEditor.text = "Финишь"
+            }
+
+        }.start()
     }
 }
