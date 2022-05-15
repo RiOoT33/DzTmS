4bcde9d40699bf47b1611a017b98f5c8dbc0edcf
Egor <niketen91@mail.ru>
2022-05-15 17:20:53 +0300

    Dz_17

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
-    val TAG = "MainActivity"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.e(TAG, "Hello World")
+    }
+    fun onClickGoTask17 (view : View){
+    val intent = Intent(this,Tack17 :: class.java)
+        startActivity(intent)
     }
 }
